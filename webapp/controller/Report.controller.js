12cce1f4d1c93854f1cc5563ef9f84dfb40b1cb9
Logali-Group <training@logaligroup.com>
2024-05-29 17:37:14 +0200

    report 2

---
+++ webapp/controller/Report.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("lgl.git2.controller.Report", {
+        onInit: function() {
+          //another change
+          //another change 2
+        }
+      });
+    }
+  );
