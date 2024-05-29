78d6ea0a303900e707c1e7a0f91cbd3b23d9324a
Logali-Group <training@logaligroup.com>
2024-05-29 16:28:53 +0200

    First commmit - initial dev

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["lgl/git2/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"lgl/git2/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
